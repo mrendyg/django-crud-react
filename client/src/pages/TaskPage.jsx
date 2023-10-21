d8ca4d764097c13dd2659f75670cc19fb9c1c55f
AndyGarcia <andyg.oyarzo@gmail.com>
2023-10-21 16:48:08 -0300

    actualizacion de comentarios

--- client/src/pages/TaskPage.jsx
+++ client/src/pages/TaskPage.jsx
@@ -1,5 +1,9 @@
 import { TasksList } from "../components/TasksList"
 
+/**Esta es una pagina para mostrar enviando a la vista pasando por 
+ * el App.jsx el cual selecciona que mostrar en la vista
+ */
+
 function TaskPage(){
     return <TasksList/>
 }
